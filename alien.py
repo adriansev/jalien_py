f4f490d591976b611ad10a95838e38a2eb777ddf
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2019-03-25 20:26:14 +0200

    make sure min version is 3.6

--- alien.py
+++ alien.py
@@ -1,7 +1,11 @@
 #!/bin/env python3
 
-import os
 import sys
+if sys.version_info[0] != 3 or sys.version_info[1] < 6:
+    print("This script requires a minimum of Python version 3.6")
+    sys.exit(1)
+
+import os
 import re
 import subprocess
 import signal
