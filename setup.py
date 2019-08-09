a0b52cc1fb35c12f71f8e78e61f8bc7484ca89cf
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2019-08-09 15:34:17 +0300

    sync license with AliROOT/AliPhysics + add preliminary files for pypy

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xjalienfs",
+    version="0.0.1",
+    author="ALICE JAliEn",
+    author_email="jalien@cern.ch",
+    description="Websocket based cli interface for ALICE experiment GRID infrastructure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.cern.ch/jalien/xjalienfs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+    ],
+)
+
