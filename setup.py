045c2cdbbc49587822c733b0fb4318d094fbfc2e
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2020-02-20 10:12:28 +0200

    setup.py: next ver is 1.0.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 
 setuptools.setup(
     name="xjalienfs",
-    version="1.0.5",
+    version="1.0.6",
     author="ALICE JAliEn",
     author_email="jalien@cern.ch",
     description="Websocket based cli interface for ALICE experiment GRID infrastructure",
