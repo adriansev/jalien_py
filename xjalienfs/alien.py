220936c16ca4a75de303ece8c2a7d5e811718b2b
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2020-03-10 21:28:36 +0200

    check pre-commit

--- xjalienfs/alien.py
+++ xjalienfs/alien.py
@@ -31,7 +31,7 @@ import async_stagger
 import websockets
 from websockets.extensions import permessage_deflate
 
-ALIENPY_VERSION_DATE = '20200310_175929'
+ALIENPY_VERSION_DATE = '20200310_182958'
 ALIENPY_EXECUTABLE = ''
 
 if sys.version_info[0] != 3 or sys.version_info[1] < 6:
