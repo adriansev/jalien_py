fbbbd3cb6427c777f91b693aa6b4cc6fc3e9f86f
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2020-05-08 16:37:01 +0300

    set current version date

--- xjalienfs/alien.py
+++ xjalienfs/alien.py
@@ -31,7 +31,7 @@ import async_stagger
 import websockets
 from websockets.extensions import permessage_deflate
 
-ALIENPY_VERSION_DATE = '20200403_234347'
+ALIENPY_VERSION_DATE = '20200508_133632'
 ALIENPY_EXECUTABLE = ''
 
 if sys.version_info[0] != 3 or sys.version_info[1] < 6:
