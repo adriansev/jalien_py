d30dde26672a850960841dd6585c5373858fb9ec
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2020-07-14 09:17:38 +0300

    alien.py update version info

--- alienpy/alien.py
+++ alienpy/alien.py
@@ -31,8 +31,8 @@ import async_stagger
 import websockets
 from websockets.extensions import permessage_deflate
 
-ALIENPY_VERSION_DATE = '20200701_093600'
-ALIENPY_VERSION_STR = '1.2.0'
+ALIENPY_VERSION_DATE = '20200714_061737'
+ALIENPY_VERSION_STR = '1.2.1'
 ALIENPY_EXECUTABLE = ''
 
 if sys.version_info[0] != 3 or sys.version_info[1] < 6:
