928b69125cd14bdc4de0304716f9057134602a40
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2020-09-04 23:56:41 +0300

    examples update

--- examples/alien_macro_lowlevel.py
+++ examples/alien_macro_lowlevel.py
@@ -12,7 +12,7 @@ j = alien.AliEn()
 
 out = j.run('pwd')
 print(f"Output of type {type(out)}")
-alien.PrintDict(out)
+alien.retf_print(out)
 print()
 
 out = j.run('pwd', 'rawstr')  # let's request to have the actual string
@@ -20,33 +20,32 @@ print(f"Output of type {type(out)}")
 print('formated:')
 alien.PrintDict(out)
 print('raw and unformated:')
-alien.PrintDict(out, opts = 'rawstr')
+print(out)
 print()
 
 out = j.run('pwd')
 print(f"Output of type {type(out)}")
 print('No metadata')
-no_meta = alien.GetDict(out, 'nometa')
-alien.PrintDict(no_meta)  # let's not print metadata
+del out.ansdict['metadata']
+alien.PrintDict(out.ansdict)  # let's not print metadata
 print('Just the content of \'results\'')
-results_list = alien.GetDict(out, 'results')
-alien.PrintDict(results_list)  # let's print just the results
+alien.PrintDict(out.ansdict['results'])  # let's print just the results
 print()
 
 print('Let\'s just log the dictionary to the logfile with warning tag')
 print(f'Check the log file {alien.DEBUG_FILE}')
-alien.PrintDict(out, 'warn')  # let's not print metadata
+alien.retf_print(out, 'warn')  # let's not print metadata
 print()
 
 out = j.run('pwd', 'nokeys')
 print(f"Output of type {type(out)}")
 print('Let\'s not receive the keys values in the server response')
-alien.PrintDict(out)
+alien.retf_print(out, 'json')
 print()
 
 out = j.run('pwd', 'nomsg')
 print(f"Output of type {type(out)}")
 print('Let\'s not receive the \'message\' values in the server response')
-alien.PrintDict(out)
+alien.retf_print(out, 'json')
 print()
 
