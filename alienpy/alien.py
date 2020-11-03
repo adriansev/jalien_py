0966e3fd9debd87403ce621a294b3e7b1089813d
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2020-11-03 17:29:55 +0200

    fix ALIENPY_KEEP_META logic

--- alienpy/alien.py
+++ alienpy/alien.py
@@ -1716,7 +1716,8 @@ if has_xrootd:
             if results['status'].error: status = PrintColor(COLORS.BRed) + 'ERROR' + PrintColor(COLORS.ColorReset)
             if results['status'].fatal: status = PrintColor(COLORS.BIRed) + 'FATAL' + PrintColor(COLORS.ColorReset)
             xrdjob = self.xrdjob_list[jobId - 1]  # joblist initilized when starting; we use the internal index to locate the job
-            if not xrdjob.isUpload and os.getenv('ALIENPY_KEEP_META'): os.remove(xrdjob.src)  # remove the created metalink
+            if not xrdjob.isUpload:
+                if not os.getenv('ALIENPY_KEEP_META')): os.remove(xrdjob.src)  # remove the created metalink
             if results['status'].ok:
                 deltaT = datetime.datetime.now().timestamp() - float(self.job_list[jobId - 1]['start'])
                 speed = float(self.job_list[jobId - 1]['bytes_total'])/deltaT
