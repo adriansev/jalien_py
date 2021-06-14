acb4880c0fcf5914b81e01a7863db2e926b34e28
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2021-06-14 18:33:57 +0300

    examples/tokeninfo.C :: workaround for root printing message

--- examples/tokeninfo.C
+++ examples/tokeninfo.C
@@ -1,4 +1,4 @@
-//usr/bin/env -S root.exe -b -q -l "${0}" ; exit $?
+//usr/bin/env -S root.exe -b -q -l -e ".x ${0}" ; exit $?
 {
 TPython::Exec("exec(\"\"\"\ntry:\n    from alienpy import alien\nexcept Exception as e:\n    try:\n        from xjalienfs import alien\n    except Exception as e:\n        print(\"Can't load alienpy, exiting...\")\n        sys.exit(1)\n\"\"\")");
 TPython::Exec("alien.retf_print(alien.DO_tokeninfo())");
