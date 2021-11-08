520c598a9631fbd1e712f470ee4ea6f59d09ad94
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2021-11-08 19:22:16 +0200

    add alienpy_help_mkdoc.py for creation of mkdoc help file

---
+++ examples/alienpy_help_mkdoc.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import shutil
+import os
+import sys
+
+try:
+    from alienpy import alien
+except Exception as e:
+    try:
+        from xjalienfs import alien
+    except Exception as e:
+        print("Can't load alienpy, exiting...")
+        sys.exit(1)
+
+
+alien.setup_logging()
+j = alien.AliEn()
+
+f_client_nowb = {**alien.AlienSessionInfo['cmd2func_map_nowb']}
+f_client_wb = {**alien.AlienSessionInfo['cmd2func_map_client']}
+f_server_wb = {**alien.AlienSessionInfo['cmd2func_map_srv']}
+
+
+def help2markup(cmd: str):
+    if not cmd: return
+    if cmd in f_client_nowb:  # these commands do NOT need wb connection
+        result = f_client_nowb[cmd](['-h'])
+    if cmd in f_client_wb:  # lookup in clien-side implementations list
+        result = f_client_wb[cmd](j.wb(), ['-h'])
+    elif cmd in f_server_wb:  # lookup in server-side list
+        result = f_server_wb[cmd](j.wb(), cmd, ['-h'])
+    else:
+        return
+    if not result.out: return
+    help_txt = result.out
+    help_txt = help_txt.replace('                ', '')
+    alien.print_out(f'\n### {cmd}\n```\n{help_txt}\n```\n')
+    if cmd in alien.AlienSessionInfo['cmd2func_map_nowb']:
+        alien.print_out('!!! warning "client-side implementation"\n\n!!! note "No connection to central servers needed"\n')
+    if cmd in alien.AlienSessionInfo['cmd2func_map_client']:
+        alien.print_out('!!! warning "client-side implementation"\n')
+    alien.print_out('---')
+
+
+
+alien.print_out('# alien.py Command reference guide')
+for cmd in alien.AlienSessionInfo['commandlist']: help2markup(cmd)
+print()
+
