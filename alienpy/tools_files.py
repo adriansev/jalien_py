225439a4d34aef4b2f38bf01f3b19e54b090d56d
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2023-06-06 18:52:49 +0300

    remove tools_files

--- alienpy/tools_files.py
+++
@@ -1,7 +0,0 @@
-'''alienpy:: file and path manipulation tools/helpers - not-networked'''
-
-
-
-if __name__ == '__main__':
-    print('This file should not be executed!', file = sys.stderr, flush = True)
-    sys.exit(95)
