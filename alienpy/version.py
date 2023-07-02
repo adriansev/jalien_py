c92208cafa8067addc3b494c93e51ab6a3054a02
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2023-07-03 00:07:24 +0300

    alien.py update version info

--- alienpy/version.py
+++ alienpy/version.py
@@ -2,9 +2,9 @@
 
 import sys
 
-ALIENPY_VERSION_HASH = '6439dfb'
-ALIENPY_VERSION_DATE = '20230607_065328'
-ALIENPY_VERSION_STR = '1.5.1'
+ALIENPY_VERSION_HASH = '34d2fa5'
+ALIENPY_VERSION_DATE = '20230702_210724'
+ALIENPY_VERSION_STR = '1.5.2'
 
 if __name__ == '__main__':
     print('This file should not be executed!', file = sys.stderr, flush = True)
