50e69264f151c4d5ab71221c21d996443c739486
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2023-07-03 15:48:54 +0300

    adapt alien_cp example

--- examples/alien_cp.py
+++ examples/alien_cp.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
+
 import sys
 
+# import alienpy functions
 try:
-    from alienpy import alien
+    from alienpy.wb_api import PrintDict, retf_print
+    from alienpy.alien import *  # nosec PYL-W0614
 except Exception:
     try:
-        from xjalienfs import alien
+        from xjalienfs.wb_api import PrintDict, retf_print
+        from xjalienfs.alien import *  # nosec PYL-W0614
     except Exception:
         print("Can't load alienpy, exiting...")
         sys.exit(1)
 
+# enable automatic pretty printing
+#from rich import print
 
-alien.setup_logging()
-wb = alien.InitConnection()
+########################################
+##   REQUIRED INITIAILZATION
+
+# Enable and setup logging
+setup_logging()  # type: ignore
+
+# Create connection to JAliEn services
+wb = InitConnection(cmdlist_func = constructCmdList)  # type: ignore
+
+##   END OF INITIALIZATION
+########################################
+
 silent = ''  # set to quiet or silet to disable output
 sys.exit(alien.retf_print(alien.DO_XrootdCp(wb, sys.argv, silent)))
+
