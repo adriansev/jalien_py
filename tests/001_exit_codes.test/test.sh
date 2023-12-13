e37858759d59a5425129a8191c75b2b9013b0896
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2023-12-13 12:01:18 +0200

    tests/001_exit_codes.test :: output in file, return message only if fail test

--- tests/001_exit_codes.test/test.sh
+++ tests/001_exit_codes.test/test.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
-rm -rf log.txt &> /dev/null
+clean_logs () { rm -f log.txt output.txt &> /dev/null; }
+
+clean_logs
 export ALIENPY_DEBUG=1 ALIENPY_DEBUG_FILE=log.txt ALIENPY_DEBUG_APPEND=1
 
-alien.py ls
-echo "ls :: Expected == 0 ; Got ${?}"
+alien.py ls &>> output.txt || { REZ="${?}"; echo "ls:: expected exitcode 0 but got ${REZ}"; exit "${REZ}"; }
 
-alien.py ls doesnotexist
-echo "ls invalid :: Expected != 0 ; Got ${?}"
+alien.py ls /doesnotexist &>> output.txt && { REZ="${?}"; echo "ls invalid dir:: expected exitcode !=0 but got ${REZ}"; exit 1; }
 
 TESTDIR="test_$(date +%s)"
-alien.py mkdir "${TESTDIR}"
-echo "mkdir :: Expected == 0 ; Got ${?}"
+alien.py mkdir "${TESTDIR}" &>> output.txt || { REZ="${?}"; echo "mkdir:: expected exitcode 0 but got ${REZ}"; exit "${REZ}"; }
 
-alien.py rm -rf "${TESTDIR}"
-echo "rm :: Expected == 0 ; Got ${?}"
+alien.py rm -rf "${TESTDIR}" &>> output.txt || { REZ="${?}"; echo "rm -rf:: expected exitcode 0 but got ${REZ}"; exit "${REZ}"; }
 
-alien.py mkdir "/fail/no/permissions/${TESTDIR}"
-echo "rm invalid :: Expected == 0 ; Got ${?}"
+alien.py mkdir "/alice/cern.ch/${TESTDIR}" &>> output.txt && { REZ="${?}"; echo "mkdir invalid dir:: expected exitcode !=0 but got ${REZ}"; exit 1; }
 
-alien.py "stat ."
-echo "stat :: Expected == 0 ; Got ${?}"
+alien.py "stat /alice/cern.ch/user/a/admin/referenceData" &>> output.txt || { REZ="${?}"; echo "stat reference dir:: expected exitcode 0 but got ${REZ}"; exit "${REZ}"; }
 
-alien.py stat /fail/no/permissions
-echo "stat invalid :: Expected != 0 ; Got ${?}"
+alien.py "stat /fail/no/valid" &>> output.txt && { REZ="${?}"; echo "stat invalid dir:: expected exitcode !=0 but got ${REZ}"; exit "${REZ}"; }
+
+clean_logs
+
